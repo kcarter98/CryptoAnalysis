3324dcf121f63494d1303fa0b3f90de76e5270be
Kaleb Carter <kcart98@att.net>
2021-11-06 15:00:11 -0600

    Completed features table

--- sql_statements.sql
+++ sql_statements.sql
@@ -4,7 +4,9 @@ SELECT * FROM exchanges;
 
 SELECT * FROM historical_market_data;
 
-SELECT * FROM ohlc;
-
 SELECT * FROM tickers;
+
+SELECT * FROM tickers JOIN coins on coins.coin_id = tickers.coin_id JOIN exchanges ON exchanges.exchange_id = tickers.exchange_id
+
+
 
